1baac6f31f78cf21338ad2f88085b3bb405aaef1
solly <solobarine@gmail.com>
2023-03-28 07:11:50 +0100

    Add Menu to Admin section

---
+++ src/views/AdminMenu.jsx
@@ -0,0 +1,13 @@
+import Menu from '../components/AdminMenu'
+import Admin from '../components/Admin'
+
+const AdminMenu = () => {
+  return (
+    <section className="page shrinkMenu">
+      <Menu />
+      <Admin />
+    </section>
+  )
+}
+
+export default AdminMenu
