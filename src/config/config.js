7bfab18624f4d4820c0a54f89969ce7bf0a17622
Solomon <solobarine@gmail.com>
2023-05-23 13:40:19 +0100

    Add Config File

--- src/config/config.js
+++ src/config/config.js
@@ -1,4 +1,5 @@
-const domain = 'http://localhost:3005'
-// const domain = 'https://api.sollyverse.render.com'
+const APP_STATE = 'development'
+
+const domain = APP_STATE === 'development' ? 'http://localhost:3005' : 'https://api-sollyverse.onrender.com'
 
 export default domain
