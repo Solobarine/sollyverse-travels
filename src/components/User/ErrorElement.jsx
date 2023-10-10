843a3cdbf1c820785c8b7dbeee9053ff50f3ca13
Solly <solobarine@gmail.com>
2023-10-10 19:55:47 +0100

    Add Error Component.

---
+++ src/components/User/ErrorElement.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ErrorElement = () => {
+  return (
+    <div>
+        <small>Sorry,An Unexpected Error Happened</small>
+    </div>
+  )
+}
+
+export default ErrorElement
