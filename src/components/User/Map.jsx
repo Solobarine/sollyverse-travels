79f252a2c42b0e28ad119242a3162d9a3c93494f
Solly <solobarine@gmail.com>
2023-10-10 19:59:35 +0100

    Create Map Component

---
+++ src/components/User/Map.jsx
@@ -0,0 +1,17 @@
+import { MapContainer, TileLayer, Marker } from "react-leaflet";
+
+const Map = (data) => {
+  const location = data.props
+  return (
+    <MapContainer center={[location.lat, location.lon]} zoom={location.zoom} scrollWheelZoom={false}>
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+        <Marker position={[location.lat, location.lon]}>
+        </Marker>
+    </MapContainer>
+  )
+}
+
+export default Map;
