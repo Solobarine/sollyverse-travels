9befc719b3aae92633b0bee16a5fa6afd1bd09aa
Solly <solobarine@gmail.com>
2023-10-10 19:22:23 +0100

    Add Admin Login Component

--- src/views/AdminLogin.jsx
+++ src/views/AdminLogin.jsx
@@ -1,32 +1,75 @@
-import { useState } from "react"
-import { Link } from "react-router-dom"
-import { useDispatch } from "react-redux"
+import { Formik, Form, Field } from 'formik'
+import { Link, useNavigate } from "react-router-dom"
+import { useDispatch, useSelector } from "react-redux"
 import { login } from '../redux/features/apiCalls/admin'
-import './AdminLogin.css'
+import './css/AdminLogin.css'
+import { loginInputs, loginSchema } from "../utils/adminSchema"
+import { useEffect } from 'react'
 
-const AdminLogin = () => {
-  const [email, setEmail] = useState("")
-  const [password, setPassword] = useState("")
+const Login = () => {
 
   const dispatch = useDispatch()
+  const navigate = useNavigate()
 
-  const user = {email, password}
+  const admin = useSelector(state => state.admin)
+
+  useEffect(() => {
+    if (admin.isLoggedIn) return navigate('/admin/dashboard')
+  }, [dispatch, admin.isLoggedIn, navigate])
+
+  console.log(admin);
+
 
   return (
     <section id="adminLogin" className="page shrinkMenu">
-      <div></div>
-        <div className="adminLogin">
-        <h1>Admin Login</h1>
-        <form>
-          <input onChange={(e) => setEmail(e.target.value)} type="email" placeholder="Enter Email"/>
-          <input onChange={(e) => setPassword(e.target.value)} type="password" placeholder="Enter Password"/>
-          <input onClick={() => dispatch(login(user))} type="submit" value="Login"/>
-          <Link>Forgot Password?</Link>
-          <Link id="adminSign" to="/admin/register">Become an Admin</Link>
-        </form>
+      <div className="adminLogin">
+        <Formik
+        initialValues={{
+          email: '',
+          password: ''
+        }}
+        validationSchema={loginSchema}
+        onSubmit={(values) => {
+          console.log(values);
+          dispatch(login(values))
+        }}
+        >
+          {
+            ({ errors, touched }) => (
+              <Form>
+                <h1>Admin SignIn</h1>
+                {admin.error ? <p id='admin_login_error'>{admin.error}</p> : null}
+                {
+                  loginInputs.map(field =>(
+                    <div key={field.id} className="form_input">
+                      <label htmlFor={field.label}>{field.label}</label>
+                      <Field
+                      key={field.id}
+                      type={field.type}
+                      name={field.name}
+                      placeholder={field.placeholder}
+                      />
+                      {
+                        errors[field.name] && touched[field.name]
+                        ? <small>{errors[field.name]}</small>
+                        : null
+                      }
+                    </div>
+                  ))
+                }
+                <input
+                type="submit"
+                value='Login'
+                onSubmit={values => dispatch(login(values))}
+                />
+              </Form>
+            )
+          }
+        </Formik>
+        <p id="link_to_register">Already an Admin, <Link to='/admin/register'>Sign Up</Link></p>
       </div>
     </section>
   )
 }
 
-export default AdminLogin
+export default Login
