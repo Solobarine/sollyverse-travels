78a4c7e368167b1ad01a7f79d52fb48214ab846c
Solly <solobarine@gmail.com>
2023-10-11 07:11:14 +0100

    Create Admin Slice

--- src/redux/features/adminReducer/admin.js
+++ src/redux/features/adminReducer/admin.js
@@ -1,51 +1,101 @@
 import { createSlice } from "@reduxjs/toolkit"
-import { signUp, login } from "../apiCalls/admin"
+import { signUp, login, verifyToken } from "../apiCalls/admin"
 
 const initialState = {
-  user: {
-    value: [],
-    status: 'idle',
-    error: []
-  }
+  user: [],
+  isLoggedIn: false,
+  status: 'idle',
+  error: ''
 }
 
 const adminSlice = createSlice({
   name: 'admin',
   initialState,
   reducers: {
-    login_admin: () => login,
-    register: () => signUp
+    logout: (state) => {
+      let result = state
+      result = initialState
+      localStorage.removeItem('authentication_token')
+      return result
+    }
   },
   extraReducers: {
     [signUp.pending]: (state) => {
-      state.user.status = 'Pending'
-      state.user.error = []
+      state.status = 'pending'
+      state.error = ''
     },
     [signUp.fulfilled]: (state, actions) => {
-      state.user.value = actions.payload.admin
-      state.user.status = 'Success'
-      state.user.error = []
+      if (actions.payload.status === 201) {
+        state.user = actions.payload.data.admin
+        localStorage.setItem('authentication_token', actions.payload.data.token)
+        state.status = 'idle'
+        state.isLoggedIn = true
+        state.error = ''
+      } else {
+        state.user = ''
+        state.status = 'failed'
+        state.isLoggedIn = false
+        state.error = actions.payload.error
+      }
     },
     [signUp.rejected]: (state, actions) => {
-      state.user.error = actions.payload.error
-      state.user.status = 'Failed'
+      if (actions.error.name !== 'TypeError') {
+        state.error = actions.error.message
+        state.isLoggedIn = false
+        state.status = 'failed'
+      }
     },
     [login.pending]: (state) => {
-      state.user.status = 'Pending'
-      state.user.error = []
+      state.status = 'pending'
+      state.error = ''
     },
     [login.fulfilled]: (state, actions) => {
-      state.user.value = actions.payload.admin
-      state.user.status = 'Success'
-      state.user.error = []
+      console.log(actions);
+      if (actions.payload.status !== 200) {
+        state.user = ''
+        state.status = 'failed'
+        state.isLoggedIn = false
+        state.error = actions.payload.error
+      } else {
+        state.user = actions.payload.data.admin
+        localStorage.setItem('authentication_token', actions.payload.data.token)
+        state.status = 'idle'
+        state.isLoggedIn = true
+        state.error = ''
+      }
     },
     [login.rejected]: (state, actions) => {
-      state.user.error = actions.payload.error
-      state.user.status = 'Failed'
+      if (actions.error.name !== 'TypeError') {
+        state.error = actions.error.message
+        state.isLoggedIn = false
+        state.status = 'failed'
+      }
+    },
+    [verifyToken.pending]: (state) => {
+      state.status = 'pending'
+      state.error = ''
+    },
+    [verifyToken.fulfilled]: (state, actions) => {
+      if (actions.payload.status === 200) {
+        state.user = actions.payload.data.admin
+        state.status = 'idle'
+        state.isLoggedIn = true
+        state.error = ''
+      } else {
+        state.user = ''
+        state.status = 'failed'
+        state.isLoggedIn = false
+        state.error = actions.payload.error
+      }
+    },
+    [verifyToken.rejected]: (state, actions) => {
+      if (actions.error.name !== 'TypeError') {
+        state.error = actions.error.message
+        state.isLoggedIn = false
+        state.status = 'failed'
+      }
     }
   }
 })
-
-export const { login_admin, register } = adminSlice.actions
 
 export default adminSlice.reducer
